7bb19f73efca34c388da0f27ade6247c803dedfc
Jeff <jeffreysaa@outlook.com>
2025-11-10 09:10:39 -0500

    feat: dbt

--- models/clients.sql
+++ models/clients.sql
@@ -8,9 +8,10 @@ select
     clientes.nombre_del_cliente as client_name,
     grupos.nombre_del_grupo as client_group,
     clientes.pais as country,
-    corregimientos.nombre_de_corregimiento as state,
-    distritos.nombre_de_distrito as district,
+    provincias.nombre_de_provincia as state,
     clientes.ciudad as city
+    distritos.nombre_de_distrito as district,
+    corregimientos.nombre_de_corregimiento as subdistrict,
 from {{ source('raw', 'clientes') }}
 left join client_data.src_ladona.grupos
     on grupos.codigo_de_grupo = clientes.codigo_de_grupo
@@ -18,11 +19,5 @@ left join client_data.src_ladona.distritos
     on distritos.codigo_de_distrito = clientes.codigo_de_distrito
 left join client_data.src_ladona.corregimientos
     on corregimientos.codigo_de_corregimiento = clientes.codigo_de_corregimiento
-
-
-
-    * pais            panama
-* provincia       chiriqui
-ciudad          david
-* distrito        bugaba
-corregimiento   concepcion
+left join client_data.src_ladona.provincias
+    on provincias.codigo_de_provincia = clientes.codigo_de_provincia
