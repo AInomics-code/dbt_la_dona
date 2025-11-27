8c815783f65f50f64a78c77424f2fefe8d26c568
Jeff <jeffreysaa@outlook.com>
2025-11-27 16:01:02 -0500

    fix: take different date for backorder model

--- models/backorder.sql
+++ models/backorder.sql
@@ -4,14 +4,14 @@
 ) }}
 
 select
-    backorder.fecha_de_la_factura as date,
+    backorder.fecha_de_pedido as date,
     backorder.numero_de_pedido as order_id,
     backorder.codigo_de_deposito as location_id,
     backorder.codigo_de_cliente as client_id,
     vendedores.nombre_de_vendedor as seller_name,
     backorder.codigo_de_producto as product_id,
-    backorder.fecha_de_la_factura as expected_delivery_date,
-    backorder.fecha_de_la_factura as actual_delivery_date,
+    backorder.fecha_de_pedido as expected_delivery_date,
+    backorder.fecha_de_pedido as actual_delivery_date,
     0 as days_delayed,
     backorder.cantidad_pedida as order_qty,
     backorder.cantidad_entregada as delivery_qty,
