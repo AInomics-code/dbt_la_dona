fe1fc398d5899c3866e335d7a0f700ff270f17ec
jeff <jeffreysaa@outlook.com>
2025-12-10 21:11:27 -0500

    feat: backorder model

--- models/backorder.sql
+++ models/backorder.sql
@@ -4,20 +4,20 @@
 ) }}
 
 select
-    backorder.fecha_de_pedido as date,
-    backorder.numero_de_pedido as order_id,
-    backorder.codigo_de_deposito as location_id,
-    backorder.codigo_de_cliente as client_id,
-    vendedores.nombre_de_vendedor as seller_name,
-    backorder.codigo_de_producto as product_id,
-    backorder.fecha_de_pedido as expected_delivery_date,
-    backorder.fecha_de_pedido as actual_delivery_date,
+    backorder."FECHA DE PEDIDO" as date,
+    backorder."NUMERO DE PEDIDO" as order_id,
+    backorder."CODIGO DE DEPOSITO" as location_id,
+    backorder."CODIGO DE CLIENTE" as client_id,
+    vendedores."NOMBRE DE VENDEDOR" as seller_name,
+    backorder."CODIGO DE PRODUCTO" as product_id,
+    backorder."FECHA DE PEDIDO" as expected_delivery_date,
+    backorder."FECHA DE PEDIDO" as actual_delivery_date,
     0 as days_delayed,
-    backorder.cantidad_pedida as order_qty,
-    backorder.cantidad_entregada as delivery_qty,
-    backorder.cantidad_pendiente as backorder_qty,
-    backorder.precio as unit_price,
-    backorder.venta as total
-from {{ source('raw', 'backorder') }}
-left join client_data.src_ladona.vendedores
-    on vendedores.codigo_de_vendedor = backorder.codigo_de_vendedor
+    backorder."CANTIDAD PEDIDA" as order_qty,
+    backorder."CANTIDAD ENTREGADA" as delivery_qty,
+    backorder."CANTIDAD PENDIENTE" as backorder_qty,
+    backorder."PRECIO" as unit_price,
+    backorder."VENTA" as total
+from client_data.dbo."BACKORDER" AS backorder
+left join client_data.dbo."VENDEDORES" AS vendedores
+    on vendedores."CODIGO DE VENDEDOR" = backorder."CODIGO DE VENDEDOR"
